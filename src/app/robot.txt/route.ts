e158998730ce69c0ccc786f0e8d1c93dc1e65e71
shaheedulaslam <shaheedulaslam061@gmail.com>
2025-08-14 21:13:03 +0400

    env updated

--- src/app/robot.txt/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  return new Response(
-    `User-agent: *
-Allow: /
-Sitemap: https://mallumeet.netlify.app/sitemap.xml`,
-    { headers: { "Content-Type": "text/plain" } }
-  );
-}
